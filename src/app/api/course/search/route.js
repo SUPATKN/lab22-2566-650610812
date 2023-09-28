97d8765a2d1ac5c69fc1a4053bf842218875851d
SUPATKN <yacht1400@gmail.com>
2023-09-28 15:34:55 +0700

    finish

--- src/app/api/course/search/route.js
+++ src/app/api/course/search/route.js
@@ -3,10 +3,21 @@ import { NextResponse } from "next/server";
 
 export const GET = async (request) => {
   const searchText = request.nextUrl.searchParams.get("searchText");
+  console.log(searchText);
   const prisma = getPrisma();
 
   //Modify following line so that it find course with "searchText" variable
-  const courses = await prisma.course.findMany({});
+  const courses = await prisma.course.findMany({
+    where: {
+      title: {
+        contains: searchText,
+        mode: "insensitive",
+      },
+    },
+    orderBy: {
+      courseNo: "asc",
+    },
+  });
 
   return NextResponse.json({ ok: true, courses });
 };
